cc54ad00e4debf8dec5edf985a715c11e17c05af
blacmarllbor0 <3100194@gmail.com>
2023-02-03 18:10:42 +0300

    функции-помощники

--- utils.go
+++ utils.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+// GenerateId генерирует новый id
+func GenerateId() string {
+	b := make([]byte, 16)
+
+	if _, err := rand.Read(b); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return fmt.Sprintf("%x", b)
+}
+
+// generateTemplate создает шаблон с переданными данными
+func generateTemplate(w http.ResponseWriter, filename, define string, data interface{}) {
+	temp, err := template.ParseFiles(filename, header, footer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := temp.ExecuteTemplate(w, define, data); err != nil {
+		log.Fatal(err)
+	}
+}
