7d07762b0a7cd9d2618939ce352385beb24d25b3
blacmarllbor0 <3100194@gmail.com>
2023-02-03 18:10:56 +0300

    модели постов

--- models/post.go
+++ models/post.go
@@ -1,1 +1,13 @@
 package models
+
+type Post struct {
+	Id      string
+	Title   string
+	Content string
+}
+
+type Posts map[string]*Post
+
+func NewPost(id, title, content string) *Post {
+	return &Post{id, title, content}
+}
